6fd0631e9577437d7f2006a27452483bc19e281a
Uerbert Almeida <uerbert88@gmail.com>
2024-10-07 22:35:07 -0300

    Conclusão

--- src/App.js
+++ src/App.js
@@ -48,7 +48,54 @@ const App = () => {
     }
 
   }
+  const handleMultNumbers = () => {
 
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('x')
+    }else {
+      const sum = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(sum))
+      setOperation('')
+    }
+
+  }
+  const handleDiviNumbers = () => {
+
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('/');
+    } else {
+      if (currentNumber === '0') {
+        alert('Divisão por zero não é permitida');
+        return;
+      }
+      const sum = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('');
+    }
+  };
+
+  const handleSquareRoot = () => {
+    if (currentNumber !== '0') {
+      const sum = Math.sqrt(Number(currentNumber));
+      setCurrentNumber(String(sum));
+      setOperation('V');
+    }
+  };
+  const handlePercentage = () => {
+    if (currentNumber !== '0') {
+      const sum = Number(firstNumber * currentNumber)/100;
+      setCurrentNumber(String(sum));
+      setOperation('%');
+    }
+  };
+
+
+   
+  
   const handleEquals = () => {
 
     if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
@@ -59,6 +106,18 @@ const App = () => {
           case '-':
             handleMinusNumbers();
             break;
+          case "x":
+           handleMultNumbers();
+            break;   
+          case "/":
+            handleDiviNumbers();  
+            break;          
+          case "V":
+            handleSquareRoot();  
+            break;          
+          case "%":
+            handlePercentage();  
+            break;          
           default: 
             break;
         }
@@ -71,27 +130,33 @@ const App = () => {
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="x"/>
-          <Button label="/"/>
+          <Button label="%" onClick={handlePercentage}/>
+          <Button label="V" onClick={handleSquareRoot}/>
           <Button label="c" onClick={handleOnClear}/>
-          <Button label="."/>
+          <Button label="/" onClick={handleDiviNumbers}/>
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={handleMinusNumbers}/>
+          <Button label="x" onClick={handleMultNumbers}/>
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={handleSumNumbers}/>
+          <Button label="-" onClick={handleMinusNumbers}/>
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="+" onClick={handleSumNumbers}/>
+        </Row>
+        <Row>
+          <Button label="" />
+          <Button label="0" onClick={() => handleAddNumber('0')}/>
+          <Button label="." onClick={() => handleAddNumber('.')} />
           <Button label="=" onClick={handleEquals}/>
         </Row>
       </Content>
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 export const ButtonContainer = styled.button`
     padding: 20px;
     border: 1px solid #CDCDCD;
-    background-color: #00AAF0;
-    color: #FFFFFF;
+    background-color: #00AAF1;
+    color: #FFFFF;
     font-size: 24px;
     font-weight: 700;
     flex: 1;
     
     &:hover {
-        opacity: 0.6;
+        opacity: 0.8;
     }
 `
